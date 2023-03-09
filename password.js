d8a175e90b5550029042ca5ca52ace5ea5b9e65a
Arda Coşkun <mail@ardacoskun.dev>
2023-03-09 11:58:18 +0300

    Update password.js

--- password.js
+++ password.js
@@ -2,11 +2,12 @@ var password = "admin";
 
  function passcheck() {
  if(document.getElementById('pass1').value != password) { 
- alert('Şifre Yanlış, Tekrar Deneyin!');
+ alert('Şifre Yanlış, Ana Sayfaya Yönlendirilyorsunuz!');
  return false;
+<meta http-equiv="refresh" content="0; URL=/"
  }
 
- if (document.getElementById('pass1').value == password) {
- alert('Şifre doğru!');
+ if (document.getElementById('pass1').value == password)
+ {('Şifre doğru!');
  }
  }
