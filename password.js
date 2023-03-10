d33c555c160983d7d2dec9c784dcbbed5526c5bd
Arda Coşkun <mail@ardacoskun.dev>
2023-03-10 22:26:45 +0300

    Add files via upload

--- password.js
+++ password.js
@@ -3,11 +3,12 @@ var password = "admin";
  function passcheck() {
  if(document.getElementById('pass1').value != password) { 
  alert('Şifre Yanlış, Ana Sayfaya Yönlendirilyorsunuz!');
+ window.location.replace("https://www.ardacoskun.dev");
  return false;
-window.location.replace("https://www.ardacoskun.dev");
  }
 
  if (document.getElementById('pass1').value == password)
  {('Şifre doğru!');
+ window.location.href = "http://www.ardacoskun.dev/admin";
  }
  }
